7df83b05e83b60ba54cd5ab0d13867be9641a256
lievaldy <lievaldy@gmail.com>
2019-06-07 13:59:57 +0700

    seeder

--- database/migrations/2018_09_07_035718_create_pro_project_table.php
+++ database/migrations/2018_09_07_035718_create_pro_project_table.php
@@ -23,7 +23,6 @@ class CreateProProjectTable extends Migration
             $table->unsignedInteger('ship_id');
             $table->unsignedInteger('customer_id');
             $table->string('name')->nullable();
-            $table->string('hull_number')->nullable();
             $table->string('person_in_charge')->nullable();
             $table->text('description')->nullable();
             $table->string('hull_number')->nullable();
